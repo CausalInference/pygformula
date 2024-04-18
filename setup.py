94201798fdd644265c973f0d272ee77860c5ab10
jing_li@hsph.harvard.edu <jing_li@hsph.harvard.edu>
2024-04-18 16:55:26 -0400

    upload setup

---
+++ setup.py
@@ -0,0 +1,15 @@
+import setuptools
+
+with open('README.md', 'r') as f:
+  long_description = f.read()
+
+setuptools.setup(
+  name='pygformula',
+  version='0.0.1',
+  maintainer='Jing Li',
+  maintainer_email='jing_li@hsph.harvard.edu',
+  description='A python implementation of the parametric g-formula',
+  long_description=long_description,
+  long_description_content_type='text/markdown',
+  packages=setuptools.find_packages()
+)
