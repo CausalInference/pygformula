7f95b611c1129eca7622fc6765dd92080c9dd9a2
jing_li@hsph.harvard.edu <jing_li@hsph.harvard.edu>
2024-04-18 17:18:44 -0400

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -38,7 +38,8 @@ sys.path.insert(0, os.path.abspath("../.."))
 
 extensions = ['sphinx.ext.autodoc',
               'sphinx.ext.autosummary',
-              'sphinx.ext.napoleon'
+              'sphinx.ext.napoleon',
+              'python_docs_theme'
 ]
 
 
