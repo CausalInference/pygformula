913325bc7c8f3685d717530340e6b96e45e4556d
jing_li@hsph.harvard.edu <jing_li@hsph.harvard.edu>
2024-04-19 01:16:17 -0400

    update conf

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,8 @@ release = '0.0.1'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('../../'))
-sys.path.insert(0, os.path.abspath(os.path.join("../..", "pygformula")))
+sys.path.insert(0, os.path.abspath('..'))
+sys.path.append(os.path.dirname(__file__))
 
 # -- General configuration ---------------------------------------------------
 
