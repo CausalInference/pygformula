991444a3e8e72a2863d56027de470edcdc7dd1d1
buehj <jing_li96@outlook.com>
2024-04-24 15:22:13 -0400

    remove unnecessary path

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -26,8 +26,6 @@ release = '0.0.1'
 
 import os
 import sys
-sys.path.insert(0, os.path.abspath('..'))
-sys.path.append(os.path.dirname(__file__))
 sys.path.insert(0, os.path.abspath('../..'))
 
 # -- General configuration ---------------------------------------------------
